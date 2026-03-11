ed0ebdf257212b7e35ab670a0a918d62f5cc8d22
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-11 22:39:02 +0900

    과제 완료

---
+++ DataProcessor/DataProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class DataProcessor
+{
+    int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    public void ForEach(Action<int> action)
+    {
+        foreach (int i in num)
+        {
+            action(i);
+        }
+    }
+    public int[] Transform(Func<int, int> transformer)
+    {
+        int[] newnum = new int[11];
+        foreach (int i in num)
+        {
+            newnum[i] = transformer(i);
+        }
+        return newnum;
+    }
+    public List<int> Filter(Func<int, bool> predicate)
+    {
+        List<int> ints = new List<int>();
+        foreach (int i in num)
+        {
+            if (predicate(i))
+            {
+                ints.Add(i);
+            }
+        }
+        return ints;
+    }
+    public void Reduce(Func<int, int, int> reducer, int initialValue)
+    {
+        int sum = initialValue;
+        foreach (int i in num)
+        {
+            sum = reducer(i, sum);
+        }
+        Console.WriteLine($"합계: {sum}");
+    }
+}
--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -2,4 +2,28 @@ using System;
 using System.Collections.Generic;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+DataProcessor data = new DataProcessor();
+Console.WriteLine("=== 원본 배열 출력 ===");
+data.ForEach(delegate (int i) { Console.Write($"{i} "); });
+Console.WriteLine();
+
+Console.WriteLine("=== 2배로 변환 ===");
+int[] num1 = data.Transform(delegate (int i) { return i * 2; });
+foreach (int i in num1)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+
+Console.WriteLine("=== 짝수만 필터링 ===");
+List<int> num2= data.Filter(delegate (int i) { return i % 2 == 0; });
+foreach (int i in num2)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+
+Console.WriteLine("=== 합계 계산 ===");
+data.Reduce(delegate (int x, int y) { return x + y; }, 0);
+
